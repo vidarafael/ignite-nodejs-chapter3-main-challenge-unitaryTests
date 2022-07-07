fd0771ed6b80d8ffdfc918b49d0f0cab5d5659a7
Rafael Freitas <vidarafael@hotmail.com>
2022-07-07 09:30:21 -0300

    feature('test unitary from authenticateUserUseCase')

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,31 +1,64 @@
+import "reflect-metadata";
+import { CreateUserUseCase } from '../createUser/CreateUserUseCase';
 import { InMemoryUsersRepository } from './../../repositories/in-memory/InMemoryUsersRepository';
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
+import { IncorrectEmailOrPasswordError } from "./IncorrectEmailOrPasswordError";
 
 let authenticateUserUseCase: AuthenticateUserUseCase;
+let createUserUseCase: CreateUserUseCase;
 let inMemoryUsersRepository: InMemoryUsersRepository;
 
 describe("Authenticate User", () => {
   beforeEach(() => {
     inMemoryUsersRepository = new InMemoryUsersRepository()
     authenticateUserUseCase = new AuthenticateUserUseCase(inMemoryUsersRepository)
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository)
   })
 
   it("should be authenticate user", async () => {
-
-    const user = await inMemoryUsersRepository.create({
+    await createUserUseCase.execute({
       name: "User test",
       email: "usertest@test.com",
       password: "1234"
     })
 
-    console.log(user)
-
     const tokenWithUser = await authenticateUserUseCase.execute({
-      email: user.email,
-      password: user.password
+      email: "usertest@test.com",
+      password: '1234'
     })
 
     expect(tokenWithUser).toHaveProperty("token")
     expect(tokenWithUser).toHaveProperty("user")
   })
+
+  it("should be not authentica user if email not exists", async () => {
+    expect(async () => {
+      await createUserUseCase.execute({
+        name: "User test",
+        email: "usertest@test.com",
+        password: "1234"
+      })
+
+      await authenticateUserUseCase.execute({
+        email: "emailNotExists@hotmail.com",
+        password: '1234'
+      })
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
+  })
+
+  it("should be not authentica user if password not exists", async () => {
+    expect(async () => {
+      await createUserUseCase.execute({
+        name: "User test",
+        email: "usertest@test.com",
+        password: "1234"
+      })
+
+      await authenticateUserUseCase.execute({
+        email: "usertest@test.com",
+        password: 'senhaNaoExiste'
+      })
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
+  })
+
 })

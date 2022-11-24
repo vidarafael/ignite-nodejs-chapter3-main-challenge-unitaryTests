0662868a57bf9116f292e6923045abb294dadf64
Rafael Freitas <vidarafael@hotmail.com>
2022-11-24 00:21:17 -0300

    feat: transfer but this implementation is not correct

--- src/routes/statements.routes.ts
+++ src/routes/statements.routes.ts
@@ -3,18 +3,21 @@ import { Router } from 'express';
 import { CreateStatementController } from '../modules/statements/useCases/createStatement/CreateStatementController';
 import { GetBalanceController } from '../modules/statements/useCases/getBalance/GetBalanceController';
 import { GetStatementOperationController } from '../modules/statements/useCases/getStatementOperation/GetStatementOperationController';
+import { TransferBalanceController } from '../modules/statements/useCases/transferStatement/TransferStatementController';
 import { ensureAuthenticated } from '../shared/infra/http/middlwares/ensureAuthenticated';
 
 const statementRouter = Router();
 const getBalanceController = new GetBalanceController();
 const createStatementController = new CreateStatementController();
 const getStatementOperationController = new GetStatementOperationController();
+const transferBalanceController = new TransferBalanceController()
 
 statementRouter.use(ensureAuthenticated);
 
 statementRouter.get('/balance', getBalanceController.execute);
 statementRouter.post('/deposit', createStatementController.execute);
 statementRouter.post('/withdraw', createStatementController.execute);
+statementRouter.post('/transfers/:user_id', transferBalanceController.execute);
 statementRouter.get('/:statement_id', getStatementOperationController.execute);
 
 export { statementRouter };
